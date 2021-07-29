979abc0b6ad921201aba819dd83801e7182a39e4
alexis zaffalon <alexiszaffalon@live.fr>
2021-07-29 19:26:48 +0200

    seed using themoviedb API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed using the given API
+
+require 'open-uri'
+require 'json'
+
+puts 'Reset Database...'
+
+Movie.destroy_all
+
+puts 'Parsing movies from themoviedb API'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+10.times do |i|
+  puts "IMPORTING MOVIES FROM PAGE #{i + 1}"
+  movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/w500"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['poster_url']}"
+    )
+  end
+end
+
+puts 'Movies created!'
